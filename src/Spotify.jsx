432d91f0442fe0cb4f66e24d8916d1e939035d92
Midori Takahashi <mintgreen7programming@gmail.com>
2023-05-20 05:59:56 +0900

    FIX:曲名検索にplaceholder

--- src/Spotify.jsx
+++ src/Spotify.jsx
@@ -62,6 +62,7 @@ return (
         value={search}
         onChange={(e) => setSearch(e.target.value)}
         className="bg-white rounded border border-gray-100 focus:ring-2 focus:ring-blue-500  focus:border-blue-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+        placeholder="曲名、アーティスト名"
       />
       <button onClick={handleSearch} type="submit" className="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
         探す
